ed5556760db081f2a9f8bbf1006642a1be29678f
Simperfy <28738855+Simperfy@users.noreply.github.com>
2021-02-08 12:39:00 +0800

    ADDED: Zoom levels UPDATED: Sample input & test file

--- index.js
+++ index.js
@@ -3,82 +3,97 @@ const fs = require('fs');
 const { loggerConfig } = require('./util');
 
 
-function img2LeafletTile ({inputFile, outputFolder, dimension = 256, shouldLog = false}) {
-  // Config
+function img2LeafletTile ({inputFile, outputFolder, zoomLevels = [], shouldLog = false}) {
+  if (zoomLevels.length <= 0) throw new Error('Please provide zoomLevels array');
+
   const logger = loggerConfig(shouldLog);
 
-  const cropDimensionWidth = dimension;
-  const cropDimensionHeight = dimension;
-  // ./Config
+  zoomLevels.forEach((zoomLevel) => {
+    const dimension = zoomLevel[1];
 
-  logger('Creating output folder.\n');
-  fs.mkdirSync(outputFolder, {recursive: true});
+    const cropDimensionWidth = dimension;
+    const cropDimensionHeight = dimension;
 
-  const paddedOutput = `${outputFolder}/padded.png`;
+    let x = 0;
+    let y = 0;
+    const z = zoomLevel[0];
 
-  let x = 0;
-  let y = 0;
+    let xLimit = 0;
+    let yLimit = 0;
 
-  let xLimit = 0;
-  let yLimit = 0;
+    logger('Creating output folder.\n');
+    fs.mkdirSync(outputFolder, {recursive: true});
 
-  const image = sharp(inputFile);
+    const paddedOutput = `${outputFolder}/padded_${z}.png`;
 
-// Set limit
-  const paddedImage = image.metadata().then((metadata) => {
-    logger(`Input fileName: ${inputFile}`);
-    logger(`Input dimension: ${metadata.width} x ${metadata.height}`);
-    const padX = getPadding(metadata.width, cropDimensionWidth);
-    const padY = getPadding(metadata.height, cropDimensionHeight);
+    const image = sharp(inputFile);
 
-    logger("added X padding: ", padX);
-    logger("added Y padding: ", padY);
-    xLimit = metadata.width + padX;
-    yLimit = metadata.height + padY;
+  // Set limit
+    const paddedImage = image.metadata().then((metadata) => {
+      logger(`Input fileName: ${inputFile}`);
+      logger(`Input dimension: ${metadata.width} x ${metadata.height}`);
+      const padX = getPadding(metadata.width, cropDimensionWidth);
+      const padY = getPadding(metadata.height, cropDimensionHeight);
 
-    logger(`New dimension: ${xLimit} x ${yLimit}`);
+      logger("added X padding: ", padX);
+      logger("added Y padding: ", padY);
+      xLimit = metadata.width + padX;
+      yLimit = metadata.height + padY;
 
-    return image.extend({
-      top: 0,
-      left: 0,
-      right: padX,
-      bottom: padY,
-      background: {r: 0, g: 0, b: 0, alpha: 0}
+      logger(`New dimension: ${xLimit} x ${yLimit}`);
+      logger('\n');
+
+      return image.extend({
+        top: 0,
+        left: 0,
+        right: padX,
+        bottom: padY,
+        background: {r: 0, g: 0, b: 0, alpha: 0}
+      })
+    });
+
+    paddedImage.then((image) => image.toFile(paddedOutput))
+    .then((_) => {
+      const newImage = sharp(paddedOutput);
+      const pendingPromises = [];
+
+      logger('\n');
+      logger('Total row: ' + xLimit / cropDimensionWidth);
+      logger('Total col: ' + yLimit / cropDimensionHeight);
+
+      let counter = 0;
+
+      for (let a = 0; a < xLimit / cropDimensionWidth; a++) {
+        for (let b = 0; b < yLimit / cropDimensionHeight; b++) {
+          const folder = `${outputFolder}/${z}/${a}/`
+          const filename = `${b}.png`;
+          const output = folder + filename;
+
+          fs.mkdirSync(folder, {recursive: true});
+          x = a * cropDimensionWidth;
+          y = b * cropDimensionHeight;
+          ++counter;
+
+          const m = newImage.extract({
+            left: x,
+            top: y,
+            width: cropDimensionWidth,
+            height: cropDimensionHeight
+          }).toFile(output);
+
+          pendingPromises.push(m);
+        }
+      }
+
+      return Promise.resolve({pendingPromises, paddedOutput, counter});
     })
+    .then(({pendingPromises, paddedOutput, counter}) => {
+      removePaddedFile(pendingPromises, paddedOutput, counter);
+    });
   });
 
-  paddedImage.then((image) => image.toFile(paddedOutput)).then((_) => {
-    const newImage = sharp(paddedOutput);
 
-    logger('\n');
-    logger('Total row: ' + xLimit / cropDimensionWidth);
-    logger('Total col: ' + yLimit / cropDimensionHeight);
-
-    let counter = 0;
-    const pendingPromises = [];
-
-    for (let a = 0; a < xLimit / cropDimensionWidth; a++) {
-      for (let b = 0; b < yLimit / cropDimensionHeight; b++) {
-        const folder = `${outputFolder}/0/${a}/`
-        const filename = `${b}.png`;
-        const output = folder + filename;
-
-        fs.mkdirSync(folder, {recursive: true});
-        x = a * cropDimensionWidth;
-        y = b * cropDimensionHeight;
-        ++counter;
-
-        const m = newImage.extract({
-          left: x,
-          top: y,
-          width: cropDimensionWidth,
-          height: cropDimensionHeight
-        }).toFile(output);
-
-        pendingPromises.push(m);
-      }
-    }
-
+  function removePaddedFile(pendingPromises, paddedOutput, counter) {
     Promise.all(pendingPromises).then((_) => {
       logger('\n');
 
@@ -89,7 +104,7 @@ function img2LeafletTile ({inputFile, outputFolder, dimension = 256, shouldLog =
 
       logger(`Generated ${counter} files.`);
     });
-  });
+  }
 
   function getPadding(dimension, cropDimension) {
     if (dimension % cropDimension === 0) return 0
--- test.js
+++ test.js
@@ -1,12 +1,18 @@
 const img2LeafletTile = require('./index');
 const path = require('path');
 
-const inputPath = path.join(__dirname, 'in/input.jpg');
+const inputPath = path.join(__dirname, 'in/input.png');
 const outputPath = path.join(__dirname, 'out');
+
+const zoomLevels = [
+    [1, 1024], // 1024 x 1024
+    [2, 512], // 512 x 512
+    [3, 256], // 256 x 256
+];
 
 img2LeafletTile({
   inputFile: inputPath,
   outputFolder: outputPath,
-  dimension: 256, // tilemap size: 256 x 256
+  zoomLevels: zoomLevels,
   shouldLog: true, // log output
 });
